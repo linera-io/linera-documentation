cc9e0b4fb70e1570aff9087cb8507aee17ffe873
ma2bd <ma2bd@users.noreply.github.com>
2025-04-04 05:21:32 +0000

    deploy: 347a6f8e591dcd8678ca51622b00ab38f92ee938

--- searcher-8b5bd394.js
+++ searcher-8b5bd394.js
@@ -468,12 +468,12 @@ window.search = window.search || {};
         showResults(true);
     }
 
-    fetch(path_to_root + 'searchindex-581415c8.json')
+    fetch(path_to_root + 'searchindex-24846419.json')
         .then(response => response.json())
         .then(json => init(json))        
         .catch(error => { // Try to load searchindex.js if fetch failed
             var script = document.createElement('script');
-            script.src = path_to_root + 'searchindex-427f9bda.js';
+            script.src = path_to_root + 'searchindex-9c055c4a.js';
             script.onload = () => init(window.search);
             document.head.appendChild(script);
         });
