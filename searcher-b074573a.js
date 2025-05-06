9ef166543121688d00eeb8691e150741e6cf28bf
ma2bd <ma2bd@users.noreply.github.com>
2025-05-06 22:43:11 +0000

    deploy: e9249735f5acc223255970075979a0ffddd7a1a7

--- searcher-b074573a.js
+++ searcher-b074573a.js
@@ -519,7 +519,7 @@ window.search = window.search || {};
         document.head.append(script);
     }
 
-    loadScript(path_to_root + 'searchindex-f52bf8d4.js', 'search-index');
+    loadScript(path_to_root + 'searchindex-1b687a32.js', 'search-index');
 
     // Exported functions
     search.hasFocus = hasFocus;
