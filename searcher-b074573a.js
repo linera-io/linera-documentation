74155810487db798116bb7f8b23944e8200afaf0
deuszx <deuszx@users.noreply.github.com>
2025-05-08 12:20:36 +0000

    deploy: 357429a03fad6fb46539ec109a810fbda16827c0

--- searcher-b074573a.js
+++ searcher-b074573a.js
@@ -519,7 +519,7 @@ window.search = window.search || {};
         document.head.append(script);
     }
 
-    loadScript(path_to_root + 'searchindex-4aad2170.js', 'search-index');
+    loadScript(path_to_root + 'searchindex-68b0ce70.js', 'search-index');
 
     // Exported functions
     search.hasFocus = hasFocus;
