b55432f192eca81ef527b9a2eb5e02fe2228c329
MathieuDutSik <MathieuDutSik@users.noreply.github.com>
2025-06-01 16:08:51 +0000

    deploy: 367e8d5e620f225b3c18e3486534bf9bcfe76349

--- searcher-b074573a.js
+++ searcher-b074573a.js
@@ -519,7 +519,7 @@ window.search = window.search || {};
         document.head.append(script);
     }
 
-    loadScript(path_to_root + 'searchindex-0864d8e3.js', 'search-index');
+    loadScript(path_to_root + 'searchindex-622e8ece.js', 'search-index');
 
     // Exported functions
     search.hasFocus = hasFocus;
