10f133c8e07faaca3abf017313efc68675249a39
ma2bd <ma2bd@users.noreply.github.com>
2025-06-03 03:55:24 +0000

    deploy: 49f51d4cd379835f561f1ad9cea44440373ed318

--- searcher-b074573a.js
+++ searcher-b074573a.js
@@ -519,7 +519,7 @@ window.search = window.search || {};
         document.head.append(script);
     }
 
-    loadScript(path_to_root + 'searchindex-cfc7d2ac.js', 'search-index');
+    loadScript(path_to_root + 'searchindex-4d7992ba.js', 'search-index');
 
     // Exported functions
     search.hasFocus = hasFocus;
