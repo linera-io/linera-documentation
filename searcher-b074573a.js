a9569807425c8e0dd240e145b5c7fdd8c17b7c6b
eldios <eldios@users.noreply.github.com>
2026-05-05 17:11:47 +0000

    deploy: 468fb0d112d94843a15c8b31844a7ae09a8b6f7e

--- searcher-b074573a.js
+++ searcher-b074573a.js
@@ -519,7 +519,7 @@ window.search = window.search || {};
         document.head.append(script);
     }
 
-    loadScript(path_to_root + 'searchindex-5abb1b29.js', 'search-index');
+    loadScript(path_to_root + 'searchindex-c9f04eba.js', 'search-index');
 
     // Exported functions
     search.hasFocus = hasFocus;
